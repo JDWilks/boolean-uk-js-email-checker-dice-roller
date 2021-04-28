84349572942630ab370852bf1e6a6aaae27ebe29
Jonathon Wilks <jonathondwilks@icloud.com>
2021-04-28 18:13:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Email checker
+// - Make a list of emails
+
+let emailList = ["jonathon@icloud.com", "rico@gmail.com", "nico@gmail.com", "sergio@gmail.com"]
+
+// - Ask the user for their email
+
+const myEmail = prompt("Please enter your email")
+
+// - Check if their email is listed
+
+
+
+// - Output a message granting them access if the email is listed
